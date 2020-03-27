6b62ccb9ab00a46331a3d755679d82b1317d5c1e
shizidy <shizizuo1990>
2020-03-27 14:07:00 +0800

    Initial Commit

---
+++ ObserverManagerTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ObserverManagerTest
+//
+//  Created by wdyzmx on 2020/3/27.
+//  Copyright © 2020 wdyzmx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
